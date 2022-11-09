564c467ed2f4e47182cf26ddb282f4d77e0f802d
Elisei Stakovskii <104836394+eistakovskii@users.noreply.github.com>
2022-11-09 17:01:05 +0300

    Create cosine_similarity.py

---
+++ NLP_tools/cosine_similarity.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def cosine_similarity(A, B):
+    '''
+    Input:
+        A: a numpy array which corresponds to a word vector
+        B: A numpy array which corresponds to a word vector
+    Output:
+        cos: numerical number representing the cosine similarity between A and B.
+    '''
+    # you have to set this variable to the true label.
+    cos = -10
+    dot = np.dot(A, B)
+    norma = np.linalg.norm(A)
+    normb = np.linalg.norm(B)
+    cos = dot / (norma * normb)
+
+    return cos
