13302cbe343ba5a7684fa3c514d0f9f92df3db66
Elisei Stakovskii <104836394+eistakovskii@users.noreply.github.com>
2022-11-21 15:36:09 +0300

    Update scr_dict_cc.py

--- DATA_AUGMENTATION/scr_dict_cc.py
+++ DATA_AUGMENTATION/scr_dict_cc.py
@@ -1,29 +1,77 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(executable_path=r'PATH TO YOUR DRIVER\chromedriver.exe') # ADD HERE THE PATH TO YOUR DRIVER
+from tqdm.auto import tqdm
 
+import re
+
+driver = webdriver.Chrome(executable_path=r'YOUR_PATH')
 
 print('\nSTARTING...')
 
-# x = driver.find_element(By.XPATH, '//*[@id="tr1"]').text
 out_list = list()
-for i in range(1, 4):
+
+not_extracted = 0
+
+block = ['idiom ', 'cloth.', 'hist.', 'art.', 'relig.', 'meteo.', 'hist.', 'anat.', 'cosmet.', 'gastr.', 'unit', 'ecol.', 'med.', 'sports', 'pol.', 'econ.', 'zool.','geogr.','sociol.', 'lit.', 'ling','RealEst.', 'naut.', 'journ.', 'lit.', 'sb.', 'ethn.', 'curr. neol.', 'sth.', 'publ.', 'comm. FoodInd.', 'educ.', 'orn.', 'neol.', 'fin.', 'mus.', 'spec.', 'psych.', 'mil.','comm.', 'automot.', 'FoodInd.', 'agr. equest.', 'constr.', 'archi.', 'agr.']
+
+for i in tqdm(range(1, 4)):
     driver.get(f"https://www.dict.cc/?s=%5Boffensive%5D&pagenum={i}")
     if i == 3:
-        for i in range(1, 47):
-            x = driver.find_element(By.XPATH, f'//*[@id="tr{i}"]/td[3]').text
-            out_list.append(x)
+        for i in range(1,42):
+            try:
+                x = driver.find_element(By.XPATH, f'//*[@id="tr{i}"]/td[2]').text
+                if x in block:
+                    continue
+                x = re.sub(r'\[[^)]*\]', '', x)
+                x = re.sub(r'\([^)]*\)', '', x)
+                x = re.sub(r'\<[^)]*\>', '', x)
+                x = re.sub(r'\{[^)]*\}', '', x)
+                x = " ".join(x.split())
+                if x.startswith('to'):
+                    x = x[3:]
+                if x in out_list:
+                    continue
+                for i in block:
+                    if i in x:
+                        x = x.replace(i, '')
+                x = " ".join(x.split())
+                x = re.sub(r'\[[^)]*\]', '', x)
+                out_list.append(x)
+            except:
+                not_extracted += 1
+                continue
     else:
         for i in range(1, 51):
-            x = driver.find_element(By.XPATH, f'//*[@id="tr{i}"]/td[3]').text
-            out_list.append(x)
+            try:
+                x = driver.find_element(By.XPATH, f'//*[@id="tr{i}"]/td[2]').text
+                x = re.sub(r'\[[^)]*\]', '', x)
+                x = re.sub(r'\([^)]*\)', '', x)
+                x = re.sub(r'\<[^)]*\>', '', x)
+                x = re.sub(r'\{[^)]*\}', '', x)
+                x = " ".join(x.split())
+                if x.startswith('to'):
+                    x = x[3:]
+                if x in out_list:
+                    continue
+                for i in block:
+                    if i in x:
+                        x = x.replace(i, '')
+                x = " ".join(x.split())
+                x = re.sub(r'\[[^)]*\]', '', x) 
+                out_list.append(x)
+            except:
+                not_extracted += 1
+                continue
+print('\nEXPORTING...') 
 
-with open('offen.txt', 'w', encoding='utf-8') as f:
+out_list = set(out_list)
+
+with open('offen_en.txt', 'w', encoding='utf-8') as f:
     for i in out_list:
         f.write(i+'\n')
+print(f'\nNUMBER OF LOST: {not_extracted}')
 
 print('\nENDING...') 
-
 
 driver.close()
