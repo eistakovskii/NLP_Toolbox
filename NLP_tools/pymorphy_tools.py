cc8a9d76f968ec40b94764fcc4077d613de5deab
Elisei Stakovskii <104836394+eistakovskii@users.noreply.github.com>
2023-01-18 17:44:46 +0300

    Update pymorphy_tools.py

--- NLP_tools/pymorphy_tools.py
+++ NLP_tools/pymorphy_tools.py
@@ -16,15 +16,15 @@ def lemmatizations(text: str) -> list:
     Input:
         text: a string to be lemmatized
     Output:
-        _ : a list of with lemmas
+        _ : list of with lemmas
     '''
-    tokens_normalized, _ = [], [] # tokens_normalized, lemma_list = [], []
+    tokens_normalized = []
     token_list = [_.text for _ in razdel.tokenize(text.lower())]
     for token in token_list:
         lemma = lemmatizer.parse(token)[0].normal_form
         tokens_normalized.append(lemma)
     
-    return " ".join(tokens_normalized).split(' ')
+    return tokens_normalized
 
 def get_num(inp_str: str) -> tuple:
     '''
