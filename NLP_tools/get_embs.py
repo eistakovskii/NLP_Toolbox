a0aab178d7a8cab6c58d80b2d478b6b634db9bce
Elisei Stakovskii <104836394+eistakovskii@users.noreply.github.com>
2023-04-26 10:59:38 +0300

    Update get_embs.py

--- NLP_tools/get_embs.py
+++ NLP_tools/get_embs.py
@@ -3,7 +3,7 @@ import torch
 from tqdm.auto import tqdm
 
 def embs_from_str_mlm(inp_str: str, extractor):
-  '''
+  """
   Use this function to find an overage of N vectors that are produced by the extractor from the input string.
 
   Input:
@@ -11,7 +11,7 @@ def embs_from_str_mlm(inp_str: str, extractor):
       extractor: pipeline of the mlm model
   Output:
       _ : returns a vector average of tokens of the input string excluding starting and trailing vectors
-  '''
+  """
   temp_vecs = extractor(inp_str)[0]
   temp_vecs = temp_vecs[1:-1]
   temp_num_vecs_len = len(temp_vecs)
@@ -23,7 +23,7 @@ def embs_from_str_mlm(inp_str: str, extractor):
   return np.average(temp_m_c_2, axis=0)
   
 def get_labse_sentence_emb(input_str: str, model_curr, tokenizer_curr):
-  '''
+  """
   Use this function to find an embedding from a whole sentence using LaBSE type of model
 
   Input:
@@ -32,7 +32,7 @@ def get_labse_sentence_emb(input_str: str, model_curr, tokenizer_curr):
       tokenizer_curr: your tokenizer object
   Output:
       _ : returns a 1 by N vector representation of the input sentence
-  '''
+  """
   encoded_input = tokenizer_curr(input_str, padding=True, truncation=True, max_length=64, return_tensors='pt')#.to(device)
   with torch.no_grad():
       model_output = model_curr(**encoded_input)
@@ -64,28 +64,33 @@ def get_cls_token(str_in: str, model_curr, tokenizer_curr):
     
     return cls_token
 
-def embs_from_str_mlm(data_in: list, model_curr, tokenizer_curr):
-  """
-  This function exploits the NSP (next sentece prediction) task with which the Bert was pretrained along the MLM task
-  and uses [CLS] token to encode the whole sentence.
-  Note that this function assumes that your using GPU
-  
-  Input:
-      data_in: your data in the form of list of strings
-      model_curr: bert model to vectorize the strings
-      tokenizer_curr: model's tokenizer
-  Output:
-      out_matrix: as the name suggests the function returns a numpy matrix with the following dimensions: (number of data samples, 768) 
-  """
-  data_len = len(data_in)
-  out_matrix = np.zeros((data_len, 768)) # Initiate with zeros a matrix with dimensions (number of train sample, 768)
-  
-  for ind, val in tqdm(enumerate(data_in)):
-      input_ids = torch.tensor(tokenizer_curr.encode(val)).unsqueeze(0).to('cuda') 
-      outputs = model_curr(input_ids) # Forward pass with the input
-      last_hidden_states = outputs[0]
-      cls_token = last_hidden_states[0][0] # Extract the CLS token and discard everything else
-      out_matrix[ind, :] = cls_token.detach().cpu().numpy() # Insert the cls token in the matrix intitiated above
-  
-  return out_matrix
+def embs_from_str_mlm(data_in: list, model_curr, tokenizer_curr, export_flag: bool = False):
+    """
+    This function exploits the NSP (next sentece prediction) task with which the Bert was pretrained along the MLM task
+    and uses [CLS] token to encode the whole sentence.
+    Note that this function assumes that your using GPU
+
+    Input:
+        data_in: your data in the form of list of strings
+        model_curr: bert model to vectorize the strings
+        tokenizer_curr: model's tokenizer
+        export_flag: specify whether you want to export the matrix
+    Output:
+        out_matrix: as the name suggests the function returns a numpy matrix with the following dimensions: (number of data samples, 768) 
+    """
+    data_len = len(data_in)
+    out_matrix = np.zeros((data_len, 768)) # Initiate with zeros a matrix with dimensions (number of train sample, 768)
+
+    for ind, val in tqdm(enumerate(data_in)):
+        input_ids = torch.tensor(tokenizer_curr.encode(val, truncation=True, max_length=512)).unsqueeze(0).to('cuda') 
+        outputs = model_curr(input_ids) # Forward pass with the input
+        last_hidden_states = outputs[0]
+        cls_token = last_hidden_states[0][0] # Extract the CLS token and discard everything else
+        out_matrix[ind, :] = cls_token.detach().cpu().numpy() # Insert the cls token in the matrix intitiated above
+
+    if export_flag: 
+        np.save(f'metrix_{data_len}.npy', out_matrix)
+        pass
+    else:
+        return out_matrix
 
