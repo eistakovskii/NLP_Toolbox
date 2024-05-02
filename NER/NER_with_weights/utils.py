b5db3b3f7abbcb8869239900b4abff7435b0fb5e
Elisei Stakovskii <104836394+eistakovskii@users.noreply.github.com>
2024-05-02 11:56:44 +0300

    Create utils.py

---
+++ NER/NER_with_weights/utils.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+import os
+import torch
+
+def set_random_seed(seed):
+    """
+    fix the random seed all around
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    os.environ["PL_GLOBAL_SEED"] = str(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
